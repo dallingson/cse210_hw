373ca7d9b072f2bf00a78271ade360f34f6ce497
GooseMeister0 <georgeson.dallin@gmail.com>
2025-01-13 20:44:16 -0700

    Prep3 and 4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number?");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1,20);
+
+        int guess = -1;
+        int attempts = 0;
+    
+    while (guess != magic_number) //loop until guess matches the magic number
+    {
+        Console.WriteLine("What is your guess?");
+        string input_1 = Console.ReadLine();
+        guess = int.Parse(input_1);
+        attempts++; //add guess attempts
+
+        if (guess < magic_number)
+        {
+            Console.WriteLine("Higher");
+        }
+            else if (guess > magic_number)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine($"You Guessed it in {attempts} attempts!");
+        }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        List<int> numbers = new List<int>(); //initialize the numbers list
+        int input;
+        int sum = 0;
+        int largest = int.MinValue;
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        do
+        {
+            Console.WriteLine("Enter number:");
+            // Read input and convert to integer
+            input = int.Parse(Console.ReadLine());
+            
+            //Append to list
+            if (input != 0)
+            {
+                numbers.Add(input);
+                sum += input;
+                if (input > largest)
+                {
+                    largest = input; 
+                }
+            }
+        } while (input != 0);
+
+        double average = numbers.Count > 0 ? (double)sum / numbers.Count : 0;
+
+        // Display the results
+        Console.WriteLine($"\nThe sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+    }   
 }
