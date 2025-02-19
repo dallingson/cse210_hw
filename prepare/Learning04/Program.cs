020937127b74209f02aa1563e09237070972820a
GooseMeister0 <georgeson.dallin@gmail.com>
2025-02-18 17:50:50 -0700

    Learning04 Activity for Week07

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.WriteLine();
+        Assignment a1 = new Assignment("Dallin Georgeson", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment a2 = new MathAssignment("Billy Smith", "Fractions", "9.2", "1-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment a3 = new WritingAssignment("George Jenkins", "American History", "The Tragedy of the Civil War");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+        Console.WriteLine();
     }
 }
