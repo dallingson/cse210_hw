9fea740c0ff90354b3c04edee7723cec71e9db25
GooseMeister0 <georgeson.dallin@gmail.com>
2025-03-15 08:48:14 -0600

    Modified EternalGoal

--- prove/Develop05/EternalGoal.cs
+++ prove/Develop05/EternalGoal.cs
@@ -3,9 +3,15 @@ public class EternalGoal : Goal
     public EternalGoal(string name, string description, int points)
         : base(name, description, points) { }
 
-    public override int RecordEvent() => _points;
+    public override int RecordEvent() 
+    
+            => _points;
 
-    public override string GetStatus() => $"[∞] {_name} - {_description}";
+    public override string GetStatus() 
+    
+        => $"[∞] {_name} - {_description}";
 
-    public override string FormatForSave() => $"Eternal|{_name}|{_description}|{_points}";
+    public override string FormatForSave() 
+        
+        => $"Eternal|{_name}|{_description}|{_points}";
 }
