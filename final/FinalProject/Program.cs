5456106a092562cc44a7bf8305be71d10a0592e1
GooseMeister0 <georgeson.dallin@gmail.com>
2025-03-28 15:53:27 -0600

    Few More Changes

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -18,19 +18,19 @@ class Program
         GroceryItem cheese = new GroceryItem("Cheese", 1, 4.49m);
         
         // Display item info
-        Console.WriteLine(milk.GetInfo());
-        Console.WriteLine(bread.GetInfo());
-        Console.WriteLine(eggs.GetInfo());
-        Console.WriteLine(cheese.GetInfo());
+        Console.WriteLine(milk.getInfo());
+        Console.WriteLine(bread.getInfo());
+        Console.WriteLine(eggs.getInfo());
+        Console.WriteLine(cheese.getInfo());
         
         // Simulate using an item
-        bread.UseItem(1);
+        bread.useItem(1);
         Console.WriteLine("After using Bread:");
-        Console.WriteLine(bread.GetInfo());
+        Console.WriteLine(bread.getInfo());
         
         // Mark an item as purchased
-        eggs.MarkAsPurchased();
+        eggs.markAsPurchased();
         Console.WriteLine("After purchasing Eggs:");
-        Console.WriteLine(eggs.GetInfo());
+        Console.WriteLine(eggs.getInfo());
     }
 }
